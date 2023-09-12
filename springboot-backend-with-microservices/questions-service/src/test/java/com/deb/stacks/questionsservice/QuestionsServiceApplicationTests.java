74edac2b4e59ead4d8b6afdf812e64a3c92623dc
debsourav33 <debsourav33@gmail.com>
2023-09-11 17:00:09 -0700

    created question microservices

---
+++ springboot-backend-with-microservices/questions-service/src/test/java/com/deb/stacks/questionsservice/QuestionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deb.stacks.questionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
