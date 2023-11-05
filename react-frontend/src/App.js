8a152d57b004755f34736879a31aa4e5c48c67b8
debsourav33 <debsourav33@gmail.com>
2023-11-04 19:36:21 -0700

    added header html

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,11 +2,13 @@ import Login from "./component/Login";
 import QuestionFeed from "./component/questions/QuestionsFeed";
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Answers from "./component/answers/AnswerList";
+import Header from "./header/Header";
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="" element={<Login/>}/>
       <Route path="/questions" element={<QuestionFeed/>}/>
---
+++ react-frontend/src/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css'; // Import the CSS file
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>HeapOverFlow - Q&A for Devs</h1>
+    </header>
+  );
+}
+
+export default Header;
